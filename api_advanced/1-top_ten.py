580c766e83a6f724644dceecc064e00d65a64fe3
blessiingab <b.ingabire1@alustudent.com>
2025-07-09 11:21:21 +0000

    api advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
-import json
-import requests
+"""Print the first 10 hot posts"""
+
 import sys
+import requests
 
 
 def top_ten(subreddit):
+    """Return the first 10 hot posts"""
 
-    """Read reddit API and return top 10 hotspots """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        list_titles = r.json()['data']['children']
-        for a in list_titles[:10]:
-            print(a['data']['title'])
-            print("OK", end="", flush=True)
-    else:
-        print("OK", end="")
+    headers = {
+        'User-Agent': 'linux:subcountscript:v1.0 (by /u/bodemurairi)'
+    }
+
+    api_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    response = requests.get(url=api_url, headers=headers, timeout=10)
+
+    response.raise_for_status()
+
+    data = response.json()
+    title_post = data['data']['children']
+
+    if not title_post:
+        print(f'No post found for {subreddit}')
+        return
+
+    if len(title_post) < 10:
+        raise KeyError(f'Not many posts for {subreddit}\n'
+                       f'Number of posts: {len(title_post)}')
+
+    for post in range(10):
+        print(title_post[post]['data']['title'])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./1-top_ten.py <subreddit>")
+        sys.exit(1)
+    subreddits = sys.argv[1]
+    top_ten(subreddits)
